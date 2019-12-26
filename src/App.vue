e1965084ba8a45db5f4695ee9985bee2f94342ed
贝贝 <593104939@qq.com>
2019-12-26 16:22:39 +0800

    图标置换

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,21 @@
       <van-tabbar-item
         replace
         to="/index"
-        icon="home-o"
+        icon="apps-o"
       >
         应用中心
       </van-tabbar-item>
       <van-tabbar-item
         replace
         to="/comroom"
-        icon="desktop-o"
+        icon="cluster-o"
       >
         机房
       </van-tabbar-item>
       <van-tabbar-item
         replace
         to="/user"
-        icon="ellipsis"
+        icon="todo-list-o"
       >
         运维工单
       </van-tabbar-item>
